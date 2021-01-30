c38b32b0822f54f8eff59c10849e58725105006f
Mainuddinhasansakib <sakibuddin831@gmail.com>
2021-01-30 18:30:55 +0600

    simplify the two section

--- main.js
+++ main.js
@@ -1,53 +1,84 @@
 document.getElementById('bookingIncrement').addEventListener('click', function(){
-    handleProductChange(true);
+    handleProductChange('firstClassInput',  true);
 })
 
 document.getElementById('bookingDecrement').addEventListener('click', function(){
-    handleProductChange(false);
+    handleProductChange('firstClassInput', false);
 
 })
-function handleProductChange(isIncrease){
-    const firstClassInput = document.getElementById('firstClassInput');
-    console.log(firstClassInput);
-    const firstClassNumber = parseFloat(firstClassInput.value);
-        let firstClassCount = firstClassNumber;
-        // const firstClassCount =firstClassNumber - 1;
-        if (isIncrease==true) {
-            firstClassCount = firstClassNumber + 1;
-        }
-        if (isIncrease==false  && firstClassNumber > 0) {
-            firstClassCount = firstClassNumber - 1;
-        }
-        firstClassInput.value = firstClassCount;
-        const firstClassTotal = firstClassCount * 150;
-        document.getElementById('subTotalInput').innerText = firstClassTotal;
-}
-
 
 document.getElementById('bookingIncrement1').addEventListener('click', function(){
-    handleProductChange1(true);
+    handleProductChange('economyClassInput', true);
 })
 
 document.getElementById('bookingDecrement1').addEventListener('click', function(){
-    handleProductChange1(false);
-
+    handleProductChange('economyClassInput', false);
 })
-function handleProductChange1(isIncrease){
-    const economyClassInput = document.getElementById('economyClassInput');
+function handleProductChange(productNum, isIncrease){
+    const productNumberInput = document.getElementById(productNum);
     // console.log(firstClassInput);
-    const economyClassNumber = parseFloat(economyClassInput.value);
-        let economyClassCount = economyClassNumber;
+    const  productNumber = parseFloat(productNumberInput.value);
+        let productNumberCount = productNumber;
         // const firstClassCount =firstClassNumber - 1;
         if (isIncrease==true) {
-            economyClassCount = economyClassNumber + 1;
+            productNumberCount = productNumber + 1;
         }
-        if (isIncrease==false  && economyClassNumber > 0){
-            economyClassCount = economyClassNumber - 1;
+        if (isIncrease==false  && productNumber > 0){
+            productNumberCount = productNumber - 1;
         }
-        economyClassInput.value = economyClassCount;
-        const economyClassTotal = economyClassCount * 100;
-        document.getElementById('subTotalInput').innerText = economyClassTotal;
+        productNumberInput.value = productNumberCount;
+        const productClassTotal = productNumberCount * 100;
+        document.getElementById('subTotalInput').innerText = productClassTotal;
 }
+
+// function handleProductChange(isIncrease){
+//     const firstClassInput = document.getElementById('firstClassInput');
+//     console.log(firstClassInput);
+//     const firstClassNumber = parseFloat(firstClassInput.value);
+//         let firstClassCount = firstClassNumber;
+//         // const firstClassCount =firstClassNumber - 1;
+//         if (isIncrease==true) {
+//             firstClassCount = firstClassNumber + 1;
+//         }
+//         if (isIncrease==false  && firstClassNumber > 0) {
+//             firstClassCount = firstClassNumber - 1;
+//         }
+//         firstClassInput.value = firstClassCount;
+//         const firstClassTotal = firstClassCount * 150;
+//         document.getElementById('subTotalInput').innerText = firstClassTotal;
+// }
+
+
+// document.getElementById('bookingIncrement1').addEventListener('click', function(){
+//     handleProductChange1(true);
+// })
+
+// document.getElementById('bookingDecrement1').addEventListener('click', function(){
+//     handleProductChange1(false);
+
+// })
+// function handleProductChange1(isIncrease){
+//     const economyClassInput = document.getElementById('economyClassInput');
+//     // console.log(firstClassInput);
+//     const economyClassNumber = parseFloat(economyClassInput.value);
+//         let economyClassCount = economyClassNumber;
+//         // const firstClassCount =firstClassNumber - 1;
+//         if (isIncrease==true) {
+//             economyClassCount = economyClassNumber + 1;
+//         }
+//         if (isIncrease==false  && economyClassNumber > 0){
+//             economyClassCount = economyClassNumber - 1;
+//         }
+//         economyClassInput.value = economyClassCount;
+//         const economyClassTotal = economyClassCount * 100;
+//         document.getElementById('subTotalInput').innerText = economyClassTotal;
+// }
+
+
+
+
+
+
 
 
 // document.getElementById('bookingIncrement').addEventListener('click', function(){
